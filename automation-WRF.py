2e81eca8640d24b1c2aead71f9f1cf404e9c2a7f
elpahlevi <elpahlevi98@gmail.com>
2020-10-06 21:40:27 +0700

    Final (Syntax formatting, for better reading)

--- automation-WRF.py
+++ automation-WRF.py
@@ -6,6 +6,7 @@ If you find any trouble, reach the author on email : elpahlevi@hotmail.com
 """
 
 import os, datetime, pycurl, certifi, subprocess, glob, re
+from os import sep
 from dateutil.relativedelta import relativedelta
 
 #set directory
@@ -25,26 +26,20 @@ prev_end_date   = dt + relativedelta(days=6)
 start_date      = dt + relativedelta(days=0)
 end_date        = dt + relativedelta(days=7)
 
-# Download GFS file of Indonesia on 0.25 degree grid resolution
-def downloadGFS(issued_time, forecast_time):
+# Download the GFS file on 0.25 degree grid resolution based on defined region
+def downloadGFS(issued_time, forecast_time, left_lon, right_lon, top_lat, bottom_lat):
     base_URL = "https://nomads.ncep.noaa.gov/cgi-bin/"
-    url_query = {
-        "elev" : "&all_lev=on", 
-        "var" : "&all_var=on", 
-        "region" : "&subregion=&leftlon=95&rightlon=141&toplat=6&bottomlat=-11"
-    }
-    
-    #Generate the urls and file name based on hours (000 to 384 hours)
+
+    #Generate the urls and file name based on forecast_time parameter
     hours = ["%03d" % hrs for hrs in range(0, forecast_time, 3)]
-    get_URL = [base_URL + "filter_gfs_0p25.pl?file=gfs.t"+ issued_time +"z.pgrb2.0p25.f"+ "%03d" % hour + url_query.get("elev") + url_query.get("var") + url_query.get("region") + "&dir=%2Fgfs." + year + month + day + "%2F" + issued_time for hour in range(0, forecast_time, 3)]
-    files_name = ["gfs_4_"+ year + month + day + "_" + issued_time + "00" +"_"+ "%03d" % hour +".grb2" for hour in range(0, forecast_time, 3)]
-    
-    #download all files using pycurl
+    get_URL = [f"{base_URL}filter_gfs_0p25.pl?file=gfs.t{issued_time}z.pgrb2.0p25.f" + "%03d" % hour + f"&all_lev=on&all_var=on&subregion=&leftlon={str(left_lon)}&rightlon={str(right_lon)}&toplat={str(top_lat)}&bottomlat={str(bottom_lat)}&dir=%2Fgfs.{year}{month}{day}%2F{issued_time}" for hour in range(0, forecast_time, 3)]
+    files_name = [f"gfs_4_{year}{month}{day}_{issued_time}00_" + "%03d" % hour + ".grb2" for hour in range(0, forecast_time, 3)]
+
+    # download all files using pycurl
     folder_location = dir + "/GFS_data/" + str(year + "-" + month + "-" + day + "/")
     if not(os.path.isdir(folder_location)): #Check if Directory exist
         os.makedirs(folder_location)
-    print("GFS files will be saved in {}".format(folder_location))
-
+    print(f"GFS files will be saved in {folder_location}")
     for url, filename, hours in zip(get_URL, files_name, hours):
         if os.path.exists(folder_location + filename): #Check if GFS file fxist
             print("File Exists")
@@ -56,8 +51,8 @@ def downloadGFS(issued_time, forecast_time):
             curl_download.setopt(curl_download.WRITEDATA, f)
             curl_download.perform()
             curl_download.close()
-        print("GFS file on " + day + "-" + month + "-" + year + " issued time " + issued_time + " and forecast time +" + str(hours) +  " has been downloaded.")
-    print("All GFS files with issued time {} downloaded".format(issued_time))
+        print(f"GFS file on {day}-{month}-{year} on issued time {issued_time} and forecast time {str(hours)} has been downloaded")
+    print(f"All GFS files with issued time {issued_time} downloaded")
 
 #read namelist.wps on WPS folder then replace start_date, GFS folder path, and end_date value based from GFS data
 def changeNamelistWPS(namelist_file):
@@ -113,45 +108,52 @@ def runWPS():
     subprocess.run([f"rm {delete_file}/FILE*"],shell=True)
     subprocess.run([f"rm {delete_file}/met_em*"], shell=True)
     subprocess.run([f"rm {delete_file}/GRIBFILE*"], shell=True)
+    
     #execute geogrid.exe
     geo_files = glob.glob(dir + "/WPS/geo_em*")
     if not(os.path.exists(geo_files[0])): #Check geo_em*
         subprocess.run("./geogrid.exe", cwd=dir + "/WPS")
         print("----------------------------Geogrid.exe executed----------------------------")
+    
     #create link to GFS data
-    gfs_date_folder = start_date + relativedelta(days=0)
-    gfs_date_folder = gfs_date_folder.strftime("%Y-%m-%d")
+    gfs_date_folder = start_date.strftime("%Y-%m-%d")
     subprocess.run(["./link_grib.csh", f"{dir}/GFS_data/{gfs_date_folder}/*"], cwd=dir + "/WPS")
     print("Link grib created")
+    
     #create link to variable tables
     if os.path.exists(dir + "/WPS/Vtable"): #Check if GFS file fxist
         print("Linked Vtable.GFS file Exists")
     else:
         subprocess.run(["ln", "-sf", "ungrib/Variable_Tables/Vtable.GFS Vtable"], cwd=dir + "/WPS/")
         print("Vtable.GFS Symlink created")
+    
     #execute ungrib.exe
     subprocess.run("./ungrib.exe", cwd=dir + "/WPS")
+    
     #execute metgrid.exe
     subprocess.run("./metgrid.exe", cwd=dir + "/WPS")
 
-def runWRF():
+def runWRF(n_processor):
     #create link to all files named met_em*
     subprocess.run([f"ln -sf {dir}/WPS/met_em* ."], shell=True, cwd=dir + "/WRF/test/em_real/")
     print("All met_em* files has been linked")
+    
     #execute real.exe
-    subprocess.run(["mpirun -np 4 ./real.exe"], shell=True, cwd=dir + "/WRF/test/em_real/")
+    subprocess.run([f"mpirun -np {str(n_processor)} ./real.exe"], shell=True, cwd=dir + "/WRF/test/em_real/")
     print("real.exe executed")
+    
     #check the output from real.exe before execute wrf.exe
     rsl_error = subprocess.check_output(["tail --lines 1 rsl.error.0000"], shell=True, cwd=dir + "/WRF/test/em_real/")
     if re.search("SUCCESS COMPLETE REAL_EM INIT", str(rsl_error)):
         #execute wrf.exe
-        subprocess.run(["mpirun -np 4 ./wrf.exe"], shell=True, cwd=dir + "/WRF/test/em_real/")
+        subprocess.run([f"mpirun -np {str(n_processor)} ./wrf.exe"], shell=True, cwd=dir + "/WRF/test/em_real/")
         print("SUCCESS COMPLETE REAL_EM INIT")
     else:
         print("Check namelist.input")
+
 #Run the Code
-downloadGFS(issued_time = "00", forecast_time = 193)
+downloadGFS(issued_time = "00", forecast_time = 193, left_lon=95, right_lon=141, top_lat=6, bottom_lat=-11)
 changeNamelistWPS(namelist_file = namelist_wps_file)
 changeNameListWRF(namelist_file = namelist_wrf_file)
 runWPS()
-runWRF()
+runWRF(n_processor=4)

26289d4d13610e0b10f700816d4fe7e92df9dccb
elpahlevi <elpahlevi98@gmail.com>
2023-01-30 20:07:14 +0700

    refactor: rewrite the code

---
+++ main.py
@@ -0,0 +1,59 @@
+""" 
+WRF-ARW Model & GFS Automation System using Python 3
+Credit : Muhamad Reza Pahlevi (@elpahlevi on Github) & Agung Baruna Setiawan Noor (@absen22 on Github)
+If you find any trouble, reach the author via email : mr.levipahlevi@gmail.com 
+"""
+
+import time
+import datetime as dt
+import utils
+
+# Arguments
+gfsout_path             = "/home/your_username/wrf_model/gfs_dataset" # Path to GFS dataset folder
+wps_path                = "/home/your_username/wrf_model/wps" # Path to compiled WPS folder
+wrf_path                = "/home/your_username/wrf_model/wrf/test/em_real" # Path to compiled WRF em_real folder
+wrfout_path             = "/home/your_username/wrf_model/wrf_output" # Path to wrfout folder
+gfs_download_increment  = 1     # set to 1 if you want to download gfs dataset for every forecast hours
+gfs_left_lon            = 90    # -180 to 180
+gfs_right_lon           = 141   # -180 to 180
+gfs_top_lat             = 8     # -90 to 90
+gfs_bottom_lat          = -13   # -90 to 90
+wrf_forecast_duration   = 1     # length of simulation days
+wrf_num_proc            = 1     # number of CPU cores will be used to execute real.exe & wrf.exe
+wrfout_domain_data      = 1     # which wrfout file will be saved
+
+# Get current time when script is executed
+start_time = time.time()
+
+# Set start_date, end_date, and forecast_time
+start_date              = dt.datetime.today() - dt.timedelta(days = 1)
+end_date                = start_date + dt.timedelta(days = wrf_forecast_duration)
+delta_in_seconds        = (end_date - start_date).total_seconds()
+gfs_forecast_time       = int(delta_in_seconds / 3600)
+
+# Path to namelist.wps, namelist.input file, and wrfout folder
+namelist_wps_file   = f"{wps_path}/namelist.wps"
+namelist_wrf_file   = f"{wrf_path}/namelist.input"
+wrfout_folder_path  = f"{wrfout_path}/" + "{date}".format(date = start_date.strftime("%Y-%m-%d"))
+
+# Execute functions
+utils.download_gfs(path = gfsout_path, start_date = start_date, issued_time = "00", forecast_time = gfs_forecast_time, increment = gfs_download_increment, left_lon = gfs_left_lon, right_lon = gfs_right_lon, top_lat = gfs_top_lat, bottom_lat = gfs_bottom_lat)
+utils.download_gfs(path = gfsout_path, start_date = start_date, issued_time = "06", forecast_time = gfs_forecast_time, increment = gfs_download_increment, left_lon = gfs_left_lon, right_lon = gfs_right_lon, top_lat = gfs_top_lat, bottom_lat = gfs_bottom_lat)
+utils.download_gfs(path = gfsout_path, start_date = start_date, issued_time = "12", forecast_time = gfs_forecast_time, increment = gfs_download_increment, left_lon = gfs_left_lon, right_lon = gfs_right_lon, top_lat = gfs_top_lat, bottom_lat = gfs_bottom_lat)
+utils.download_gfs(path = gfsout_path, start_date = start_date, issued_time = "18", forecast_time = gfs_forecast_time, increment = gfs_download_increment, left_lon = gfs_left_lon, right_lon = gfs_right_lon, top_lat = gfs_top_lat, bottom_lat = gfs_bottom_lat)
+utils.change_namelist_wps(start_date = start_date, end_date = end_date, namelist_path = namelist_wps_file)
+utils.change_namelist_wrf(start_date = start_date, end_date = end_date, namelist_path = namelist_wrf_file)
+utils.run_wps(wps_path = wps_path, gfsout_path = gfsout_path, start_date = start_date)
+utils.run_wrf(wps_path = wps_path, wrf_path = wrf_path, np = wrf_num_proc)
+utils.move_output(wrf_path = wrf_path, domain = wrfout_domain_data, output_path = wrfout_folder_path)
+
+# Calculate execution time
+end_time = time.time()
+if end_time - start_time < 3600:
+    execution_duration = ("%1d" % ((end_time - start_time) / 60))
+    print(f"Process completed in {execution_duration} minutes")
+    exit(0)
+else:
+    execution_duration = ("%1d" % ((end_time - start_time) / 3600))
+    print(f"Process complete in {execution_duration} hours")
+    exit(0)
---
+++ utils.py
@@ -0,0 +1,143 @@
+import os
+import sys
+import re
+import subprocess
+from datetime import date
+
+# Download the GFS data on 0.25 degree grid based on defined arguments
+def download_gfs(path: str, start_date: date, issued_time: str, forecast_time: int, increment: int, left_lon: float, right_lon: float, top_lat: float, bottom_lat: float):
+    if forecast_time > 384:
+        sys.exit("Error: Forecast time can't be more than 384")
+    
+    base_url = "https://nomads.ncep.noaa.gov/cgi-bin/"
+    year  = str(start_date.year)
+    month = str("%02d" % (start_date.month))
+    day   = str("%02d" % (start_date.day))
+
+    # Generate URL and filename based on length of forecast time
+    list_hours = ["%03d" % hrs for hrs in range(0, forecast_time + 1, increment)]
+    list_url = [f"{base_url}filter_gfs_0p25.pl?file=gfs.t{issued_time}z.pgrb2.0p25.f" + "%03d" % hour + f"&all_lev=on&all_var=on&subregion=&leftlon={str(left_lon)}&rightlon={str(right_lon)}&toplat={str(top_lat)}&bottomlat={str(bottom_lat)}&dir=%2Fgfs.{year}{month}{day}%2F{issued_time}%2Fatmos" for hour in range(0, forecast_time + 1, increment)]
+    list_filename = [f"gfs_4_{year}{month}{day}_{issued_time}00_" + "%03d" % hour + ".grb2" for hour in range(0, forecast_time + 1, increment)]
+
+    # create a folder path
+    folder_location = f"{path}/{year}-{month}-{day}/"
+
+    # Check if Directory exist
+    if not(os.path.isdir(folder_location)):
+        os.makedirs(folder_location)
+    print(f"GFS files will be saved in {folder_location}")
+
+    # Download GFS data using curl
+    for url, filename, hours in zip(list_url, list_filename, list_hours):
+        # Check if GFS file exist
+        file_path = folder_location + filename
+        if os.path.exists(file_path):
+            print("File Exist")
+            continue
+        # Execute curl using subprocess
+        subprocess.run([f"curl \"{url}\" -o {file_path}"], shell=True)
+        print(f"GFS data on {day}-{month}-{year} at issued time {issued_time} and forecast time {hours} has been downloaded")
+    print(f"All GFS files with issued time {issued_time} has been downloaded")
+
+# Read namelist.wps file then replace start_date and end_date value
+def change_namelist_wps(start_date: date, end_date: date, namelist_path: str):
+    # parse into YYYY-mm-dd format (eg: 2023-01-27)
+    current_start  = start_date.strftime("%Y-%m-%d")
+    current_end    = end_date.strftime("%Y-%m-%d")
+
+    # Use sed command to change the date value (Credit to : absen22 @github)
+    subprocess.run([f"sed -i.backup -r '/^ start_date/s/'2000-01-01_00:00:00'/'{current_start}_00:00:00'/g' {namelist_path}"], shell=True)
+    subprocess.run([f"sed -i.backup -r '/^ end_date/s/'2000-01-01_00:00:00'/'{current_end}_00:00:00'/g' {namelist_path}"], shell=True)
+
+    print("namelist.wps file has been updated!") 
+    print(f"start_date value changed to {current_start}")
+    print(f"end_date value changed to {current_end}")
+
+# Read namelist.input file then replace start_year, start_month, start_day, start_hour, end_year, end_month, end_day, end_hour
+def change_namelist_wrf(start_date: date, end_date: date, namelist_path: str):
+    # split start_date variable into year, month, and day
+    start_year_wrf = str(start_date.year)
+    start_month_wrf = str("%02d" % start_date.month)
+    start_day_wrf = str("%02d" % start_date.day)
+    
+    # split end_date variable into year, month, day
+    end_year_wrf = str(end_date.year)
+    end_month_wrf = str("%02d" % end_date.month)
+    end_day_wrf = str("%02d" % end_date.day)
+
+    # Use sed command to change the date value (Credit to : absen22 @github)
+    # change run_days value
+    subprocess.run([f"sed -i.backup -r '/^ run_days/s/'0'/'{start_day_wrf}'/g' {namelist_path}"], shell=True)
+
+    # change start values
+    subprocess.run([f"sed -i.backup -r '/^ start_year/s/'2000'/'{start_year_wrf}'/g' {namelist_path}"], shell=True)
+    subprocess.run([f"sed -i.backup -r '/^ start_month/s/'01'/'{start_month_wrf}'/g' {namelist_path}"], shell=True)
+    subprocess.run([f"sed -i.backup -r '/^ start_day/s/'01'/'{start_day_wrf}'/g' {namelist_path}"], shell=True)
+    
+    # change end values
+    subprocess.run([f"sed -i.backup -r '/^ end_year/s/'2000'/'{end_year_wrf}'/g' {namelist_path}"], shell=True)
+    subprocess.run([f"sed -i.backup -r '/^ end_month/s/'01'/'{end_month_wrf}'/g' {namelist_path}"], shell=True)
+    subprocess.run([f"sed -i.backup -r '/^ end_day/s/'01'/'{end_day_wrf}'/g' {namelist_path}"], shell=True)
+    
+    print("namelist.input file has been updated!")
+    print("The model will simulate from {start} to {end}".format(start = start_date.strftime("%d-%m-%Y"), end = end_date.strftime("%d-%m-%Y")))
+
+# Run a step to generate met_em file
+def run_wps(wps_path: str, gfsout_path: str, start_date: date):
+    # Delete FILE* and met_em* files from previous run
+    subprocess.run([f"rm {wps_path}/FILE*"],shell=True)
+    subprocess.run([f"rm {wps_path}/met_em*"], shell=True)
+    subprocess.run([f"rm {wps_path}/GRIBFILE*"], shell=True)
+    subprocess.run([f"rm {wps_path}/geo_em*"], shell=True)
+    
+    #execute geogrid.exe
+    subprocess.run("./geogrid.exe", cwd=wps_path)
+    print("----------------------------Geogrid.exe executed----------------------------")
+    
+    #create link to GFS data
+    gfs_date_folder = start_date.strftime("%Y-%m-%d")
+    subprocess.run(["./link_grib.csh", f"{gfsout_path}/{gfs_date_folder}/*"], cwd=wps_path)
+    print("Link grib created")
+    
+    # create link to variable tables
+    if os.path.exists(f"{wps_path}/Vtable"):
+        print("Linked Vtable.GFS file Exists")
+    else:
+        subprocess.run(["ln", "-sf" ,f"{wps_path}/ungrib/Variable_Tables/Vtable.GFS", "Vtable"], cwd=wps_path)
+        print("Vtable.GFS Symlink created")
+    
+    #execute ungrib.exe
+    subprocess.run("./ungrib.exe", cwd=wps_path)
+    
+    #execute metgrid.exe
+    subprocess.run("./metgrid.exe", cwd=wps_path)
+
+# Run WRF model
+def run_wrf(wps_path: str, wrf_path: str, np: int):
+    # Delete the met_em files from last simulation
+    subprocess.run([f"rm {wrf_path}/met_em*"], shell=True)
+    
+    # Create symlink to all files named met_em*
+    subprocess.run([f"ln -sf {wps_path}/met_em* ."], shell=True, cwd=wrf_path)
+    print("All met_em* files has been linked")
+    
+    # Execute real.exe
+    subprocess.run([f"mpirun -np {np} ./real.exe"], shell=True, cwd=wrf_path)
+    print("real.exe executed")
+    
+    # Check the output from real.exe before execute wrf.exe
+    rsl_error = subprocess.check_output(["tail --lines 1 rsl.error.0000"], shell=True, cwd=wrf_path)
+    if re.search("SUCCESS COMPLETE REAL_EM INIT", str(rsl_error)):
+        # Execute wrf.exe
+        subprocess.run([f"mpirun -np {np} ./wrf.exe"], shell=True, cwd=wrf_path)
+        print("SUCCESS COMPLETE REAL_EM INIT")
+    else:
+        sys.exit("Check namelist.input")
+
+# Move the output to specific folder based on selected domain
+def move_output(wrf_path: str, domain: int, output_path: str):
+    if not(os.path.isdir(output_path)):
+        os.makedirs(output_path)
+    
+    subprocess.run([f"mv {wrf_path}/wrfout_d0{domain}* {output_path}/wrfout_d0{domain}.nc"], shell=True, cwd=wrf_path)
+    print(f"WRF simulation files on domain {domain} has been saved to {output_path}")
